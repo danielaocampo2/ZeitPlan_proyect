f8b9ccc6bf4a5b223a50bb2a0b9a6369f73439e9
danielaocampo2 <danielaocampoo@gmail.com>
2022-04-04 00:58:40 +0200

    Obtiene datos del usuario.

--- app/src/main/java/com/example/zeitplan_proyect/MainActivity.java
+++ app/src/main/java/com/example/zeitplan_proyect/MainActivity.java
@@ -3,14 +3,43 @@ package com.example.zeitplan_proyect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Variable para gestionar FirebaseAuth
+    private FirebaseAuth mAuth;
 
+    private TextView userNombre,userEmail,userID;
+    private CircleImageView userImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userNombre = findViewById(R.id.userNombre);
+        userEmail = findViewById(R.id.userEmail);
+        userID = findViewById(R.id.userId);
+        userImg = findViewById(R.id.userImagen);
+
+        //Iniciar firebase Auth
+        mAuth=FirebaseAuth.getInstance();
+        FirebaseUser currentUser= mAuth.getCurrentUser();
+
+        //Establecemos los campos
+        userNombre.setText(currentUser.getDisplayName());
+        userEmail.setText(currentUser.getEmail());
+        userID.setText(currentUser.getUid());
+
+        //Cargar imagen con glide
+        Glide.with(this).load(currentUser.getPhotoUrl()).into(userImg);
+
     }
 }
