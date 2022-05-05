f4c1e3f071e6098f7feda0b2736e79bfa85850fa
sfpol <pol.sanchez.forns@gmail.com>
2022-05-05 21:02:13 +0200

    · Ahora en 'Nuevo evento' se puede escoger el dia y la hora del evento.

--- app/src/main/java/com/example/zeitplan_proyect/EventEditActivity.java
+++ app/src/main/java/com/example/zeitplan_proyect/EventEditActivity.java
@@ -3,21 +3,32 @@ package com.example.zeitplan_proyect;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Calendar;
+
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class EventEditActivity extends AppCompatActivity {
 
     private EditText eventNameET;
     private TextView eventDateTV, eventTimeTV;
+    TimePickerDialog.OnTimeSetListener setListenerTimeEvent;
+    DatePickerDialog.OnDateSetListener setListenerDateEvent;
 
     private LocalTime time;
+    private LocalDate date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,9 +37,49 @@ public class EventEditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event_edit);
         initWidgets();
         time = LocalTime.now();
-        eventDateTV.setText("Data: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
-        eventTimeTV.setText("Hora: " + CalendarUtils.formattedTime(time));
+        eventTimeTV.setText(CalendarUtils.formattedShortTime(time));
+        date = CalendarUtils.selectedDate;
+        eventDateTV.setText(date.getDayOfMonth() + "/" + date.getMonthValue() + "/" + date.getYear());
+
+        eventDateTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePicker = new DatePickerDialog(
+                        EventEditActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, setListenerDateEvent, date.getYear(), date.getMonthValue() - 1, date.getDayOfMonth());
+                datePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePicker.show();
+
+            }
+        });
+        setListenerDateEvent = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                String day_month_year = datePicker.getDayOfMonth() + "/" + (datePicker.getMonth()+1) + "/" + datePicker.getYear();
+                eventDateTV.setText(day_month_year);
+                date = LocalDate.of(datePicker.getYear(), datePicker.getMonth() + 1, datePicker.getDayOfMonth());
+            }
+        };
+
+        eventTimeTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePicker = new TimePickerDialog(
+                            EventEditActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, setListenerTimeEvent, time.getHour(), time.getMinute(), true);
+                timePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePicker.show();
+            }
+        });
+        setListenerTimeEvent = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int e) {
+                String hour_time = timePicker.getHour() + ":" + timePicker.getMinute();
+                eventTimeTV.setText(hour_time);
+                time = LocalTime.of(timePicker.getHour(), timePicker.getMinute());
+            }
+        };
     }
+
+
 
     private void initWidgets()
     {
@@ -40,7 +91,8 @@ public class EventEditActivity extends AppCompatActivity {
     public void saveEventAction(View view)
     {
         String eventName = eventNameET.getText().toString();
-        Event newEvent = new Event(eventName, CalendarUtils.selectedDate, time);
+        if(eventName.equals("")) eventName = "Evento " + (Event.eventsList.size()+1);
+        Event newEvent = new Event(eventName, date, time);
         Event.eventsList.add(newEvent);
         finish();
     }
