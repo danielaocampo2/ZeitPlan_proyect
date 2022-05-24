c1cf0232332f7d4e0441fb5682d399db63cd8e4f
danielaocampo2 <danielaocampoo@gmail.com>
2022-05-24 15:30:02 +0200

    Recuperar contraseña

--- app/src/main/java/com/example/zeitplan_proyect/DataBase/Firebase.java
+++ app/src/main/java/com/example/zeitplan_proyect/DataBase/Firebase.java
@@ -45,7 +45,7 @@ public class Firebase {
 
     public FirebaseAuth mAuth=FirebaseAuth.getInstance();
     public FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
-    public User usuario=User.getInstance();;
+    public User usuario=User.getInstance();
     //CollectionReference user = mFirestore.collection("user");
 
     //Variables opcionales para cerrar sesión en  de google
