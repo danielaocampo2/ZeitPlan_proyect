9387964fb5494f48cde111b751cd08a7d951b069
danielaocampo2 <danielaocampoo@gmail.com>
2022-05-25 11:20:40 +0200

    inicio de recordar evento

---
+++ app/src/main/java/com/example/zeitplan_proyect/vista/Recordar.java
@@ -0,0 +1,16 @@
+package com.example.zeitplan_proyect.vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.zeitplan_proyect.R;
+
+public class Recordar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recordar);
+    }
+}
