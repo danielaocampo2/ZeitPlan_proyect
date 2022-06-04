051c975c8744775d4b5f671b8111c2667203d8e9
sfpol <pol.sanchez.forns@gmail.com>
2022-06-04 18:53:08 +0200

    Se ve mejor la lista i no funciona la vista semanal

--- app/src/main/java/com/example/zeitplan_proyect/vista/WeekViewActivity.java
+++ app/src/main/java/com/example/zeitplan_proyect/vista/WeekViewActivity.java
@@ -1,6 +1,7 @@
 package com.example.zeitplan_proyect.vista;
 
 
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,22 +20,35 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.zeitplan_proyect.DataBase.Firebase;
+import com.example.zeitplan_proyect.DataBase.MyAdapter;
 import com.example.zeitplan_proyect.MainActivity2;
 import com.example.zeitplan_proyect.model.Event;
 import com.example.zeitplan_proyect.R;
 import com.example.zeitplan_proyect.presenter.PresenterCalendarUtils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class WeekViewActivity extends Fragment implements CalendarAdapter.OnItemListener {
+
     private TextView monthYearText;
     private RecyclerView calendarRV;
-    private ListView eventListView;
+    private RecyclerView eventRV;
     private Button prevWeekAction,nextWeekAction,dailyAction, calendarAction, llistaAction;
 
+    ArrayList<Event> eventos;
+    MyAdapter eAdapter;
+    FirebaseFirestore mFirestore;
+    Firebase db = new Firebase();
     PresenterCalendarUtils PresCal;
 
     public WeekViewActivity() {
@@ -47,7 +62,7 @@ public class WeekViewActivity extends Fragment implements CalendarAdapter.OnItem
 
         calendarRV = view.findViewById(R.id.calendarRV);
         monthYearText = view.findViewById(R.id.monthYearTV);
-        eventListView = view.findViewById(R.id.eventListView);
+        eventRV = view.findViewById(R.id.eventRV);
         prevWeekAction = view.findViewById(R.id.prevWeekAction);
         nextWeekAction = view.findViewById(R.id.nextWeekAction);
         calendarAction=view.findViewById(R.id.calendarButt);
@@ -58,7 +73,13 @@ public class WeekViewActivity extends Fragment implements CalendarAdapter.OnItem
 
         PresCal = PresenterCalendarUtils.getInstance();
 
+        mFirestore = FirebaseFirestore.getInstance();
+        eventos = new ArrayList<>();
+        eAdapter = new MyAdapter(getContext(), eventos);
+        eventRV.setAdapter(eAdapter);
+
         setWeekView();
+        setEventAdapter();
 
         prevWeekAction.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -102,8 +123,6 @@ public class WeekViewActivity extends Fragment implements CalendarAdapter.OnItem
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 7);
         calendarRV.setLayoutManager(layoutManager);
         calendarRV.setAdapter(calendarAdapter);
-        setEventAdapter();
-
     }
 
 
@@ -133,14 +152,26 @@ public class WeekViewActivity extends Fragment implements CalendarAdapter.OnItem
     public void onResume()
     {
         super.onResume();
-        setEventAdapter();
     }
 
     private void setEventAdapter()
     {
-        ArrayList<Event> dailyEvents = Event.eventsForDate(PresCal.getSelectedDate());
-        EventAdapter eventAdapter = new EventAdapter(getActivity().getApplicationContext(), dailyEvents);
-        eventListView.setAdapter(eventAdapter);
+        mFirestore.collection("evento").whereEqualTo("idUser",db.getIdUser())
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if(error != null ){
+                            Log.e("Firestore error", error.getMessage() );
+                            return;
+                        }
+                        for(DocumentChange dc: value.getDocumentChanges()){
+                            if(dc.getType() == DocumentChange.Type.ADDED){
+                                eventos.add(dc.getDocument().toObject(Event.class));
+                            }
+                            eAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
     }
 
     public void calendarAction(View view) {
