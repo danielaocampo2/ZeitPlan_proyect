7e8b325de9074338f1c2f33c1092362552d0d0bf
sfpol <pol.sanchez.forns@gmail.com>
2022-06-12 01:14:10 +0200

    Hay un poco de codigo en este error (Pull hecho)

--- app/src/main/java/com/example/zeitplan_proyect/DataBase/MyAdapter.java
+++ app/src/main/java/com/example/zeitplan_proyect/DataBase/MyAdapter.java
@@ -1,11 +1,16 @@
 package com.example.zeitplan_proyect.DataBase;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Build;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
